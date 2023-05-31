e7bd41b97db867fbe442a923e42d502dfdd4719a
abhi360g <abhi360g@gmail.com>
2023-05-30 22:40:25 -0400

    First commit

---
+++ src/main/java/com/sao/HausofSao/HausOfSaoApplication.java
@@ -0,0 +1,13 @@
+package com.sao.HausofSao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HausOfSaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HausOfSaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sao/HausofSao/HausOfSaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sao.HausofSao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HausOfSaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
